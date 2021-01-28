c981d8ddff58538c888ed04cdb896911c10c25cc
caoshuiming <2639067440@qq.com>
2021-01-28 10:25:58 +0800

    20210128morning

--- src/main/java/com/csm/application/SpringApplication.java
+++ src/main/java/com/csm/application/SpringApplication.java
@@ -5,10 +5,16 @@ import com.csm.dao.UserDao;
 import com.csm.dao.impl.UserDaoImpl;
 import com.csm.service.UserService;
 import com.csm.service.impl.UserServiceImpl;
+import com.csm.servlet.HelloServlet;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.core.StandardServer;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SpringApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LifecycleException {
 
 //        System.out.println(SingleTonUser.getInstance());
 //        System.out.println(SingleTonUser.getInstance());
@@ -32,8 +38,28 @@ public class SpringApplication {
 //        System.out.println(carDao.getClass().getSuperclass());
 //        carDao.drive();
 
-        CarDao carDao = context.getBean(CarDao.class);
-        carDao.drive();
-        carDao.stop();
+//        CarDao carDao = context.getBean(CarDao.class);
+//        carDao.drive();
+//        carDao.stop();
+
+        Tomcat tomcat = new Tomcat();
+        Connector connector = new Connector();
+        connector.setPort(8888);
+        connector.setURIEncoding("UTF-8");
+//        tomcat.setBaseDir();  // 设置工作目录，这个工作目录可以是任意目录，主要是tomcat需要这 个目录来记录一些东西，比如记录word信息，日志信息（如果配置了日志的话），以及临时文件存储等。
+//        tomcat.getHost().setAppBase(); // 设置我们的项目程序所在的appbase，即放项目代码的地方。在通常的tomcat配置中，这个目录一般是webapps。
+        tomcat.getService().addConnector(connector);
+
+//        StandardServer server = (StandardServer) tomcat.getServer();
+        StandardContext standardContext = new StandardContext();
+        standardContext.addLifecycleListener(new Tomcat.FixContextListener());
+        standardContext.setPath("/mvc");
+
+        tomcat.getHost().addChild(standardContext);
+        tomcat.addServlet("/mvc", "helloServlet", new HelloServlet());
+        standardContext.addServletMappingDecoded("/hello", "helloServlet");
+
+        tomcat.start();
+        tomcat.getServer().await(); // 维持tomcat服务，否则tomcat一启动就会关闭
     }
 }
---
+++ src/main/java/com/csm/servlet/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.csm.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter pw = resp.getWriter();
+        pw.write("hello world");
+        pw.flush();
+        pw.close();
+    }
+
+}
